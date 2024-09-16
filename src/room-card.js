6aa2661c2a29efb23f7dc264f165f6efb9645883
Eleazar Hernández <eleherbri@gmail.com>
2024-09-16 13:33:57 +0200

    unzipping

--- src/room-card.js
+++ src/room-card.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html, css } from 'https://unpkg.com/lit-element@2.4.0/lit-element.js?module';
 
 class RoomCard extends LitElement {
   static get properties() {
